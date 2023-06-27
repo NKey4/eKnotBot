bd2f0767ade01998fd296d71cc0f7f07d5869613
Draconi <erkin.klyshev@mail.ru>
2023-06-27 11:19:59 +0600

    Добавление статусов

--- constants.js
+++ constants.js
@@ -1,6 +1,10 @@
 const STATUS = [
     { key: '1', value: 'Новая' },
-    { key: '2', value: 'На рассмотрении' }
+    { key: '2', value: 'В работе' },
+    { key: '3', value: 'Исполнена' },
+    { key: '4', value: 'Закрыта' },
+    { key: '5', value: 'Отклонена' },
+    { key: '6', value: 'Отменена' }
   ];
 module.exports = {
     STATUS
