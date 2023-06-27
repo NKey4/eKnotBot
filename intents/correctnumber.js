4722628dd1ac0abe943c88cc1d84d775fb8931d9
Draconi <erkin.klyshev@mail.ru>
2023-06-27 15:03:46 +0600

    мелкие изменения

--- intents/correctnumber.js
+++ intents/correctnumber.js
@@ -10,15 +10,11 @@ const correctnumber = async (res, queryResult, user_id) => {
       6
     )} ${digitsOnly.slice(6, 8)} ${digitsOnly.slice(8)}`;
   } else if (digitsOnly.length === 11 && /^[78]/.test(digitsOnly)) {
-    digitsOnly = `+7 ${digitsOnly.slice(1, 4)} ${digitsOnly.slice(
-      4,
-      7
-    )} ${digitsOnly.slice(7, 9)} ${digitsOnly.slice(9)}`;
+    digitsOnly = `+7 ${digitsOnly.slice(1, 4)} ${digitsOnly.slice(4,7)} ${digitsOnly.slice(7, 9)} ${digitsOnly.slice(9)}`;
   }
 
   const userRef = db.collection("users").doc(user_id);
   const result = await userRef.get();
-
   if (result.exists && result.data().phoneNumber === digitsOnly) {
     res.send({ fulfillmentText: `С возвращением, ${result.data().name}` });
   } else {
--- intents/getApplications.js
+++ intents/getApplications.js
@@ -1,5 +1,5 @@
 const { db } = require("../firebase");
-const {STATUS} = require("../constants");
+const {STATUS} = require("../constants/constants");
 const getApplications = async (res, user_id) => {
   const appRef = db.collection("applications").doc(user_id);
   const result = await appRef.get();
@@ -11,7 +11,6 @@ const getApplications = async (res, user_id) => {
     ([key, value]) =>
       `Заявка: ${key}, по адресу: ${value.address}, cтатус \"${ STATUS.find(item => item.key === value.status)?.value}\".`
   );
-  //STATUS.find(item => item.key === value.status)?.value
   const fulfillmentText = responses.join("\n");
   res.send({ fulfillmentText });
 };
