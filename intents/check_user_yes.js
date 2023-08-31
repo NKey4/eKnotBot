d453524d970f0b1e1d4811e8a66189ffe85228f9
Draconi <erkin.klyshev@mail.ru>
2023-08-31 17:10:56 +0600

    Последние изменения

--- intents/check_user_yes.js
+++ intents/check_user_yes.js
@@ -1,16 +1,32 @@
 const { db } = require("../firebase");
 const { format_number } = require("../intents/format_number");
+const axios = require("axios");
+require("dotenv").config();
 
 const check_user_yes = async (res, queryResult, user_id) => {
     const { phoneNumber} = queryResult.outputContexts[1].parameters;
-
     const digitsOnlyPhoneNum = format_number(phoneNumber);
 
     const collectionRef = db.collection("users").doc(user_id);
     const updateData = {};
     updateData['phoneNumber'] = digitsOnlyPhoneNum;
-    await collectionRef.update(updateData);
-    res.send({fulfillmentText: "Введите код, который был сформирован в приложении E-knot (Енот)."})
+    await collectionRef.update(updateData)
+    .then(()=>{
+        console.log("метод update (check_user_yes) выполнен")
+    })
+    .catch((error)=>{
+        console.error('Ошибка в выполнении метода update (check_user_yes):', error);
+    });
+    try{
+        const data = {
+            phoneNumber: digitsOnlyPhoneNum
+        };
+        await axios.post(`${process.env.SEND_CODE_URL}`,data);
+        res.send({fulfillmentText: "Введите код, который был сформирован в приложении E-knot (Енот)."})
+    } catch (error){
+        console.log("Ошибка сервера (check_user_yes)");
+        return res.sendStatus(500);
+    }
 }
 
 module.exports = check_user_yes;
--- intents/check_user_yes_code.js
+++ intents/check_user_yes_code.js
@@ -8,32 +8,38 @@ const check_user_yes_code = async (res, queryResult, user_id) => {
   const digitsOnlyCode = format_code(code);
   const userRef = db.collection("users").doc(user_id);
   const user = await userRef.get();
+  let eknotdata = "";
+  try{
+    const data = {
+      yandexId: user_id,
+      code: digitsOnlyCode,
+      phoneNumber: digitsOnlyPhoneNum
+  };
+    eknotdata = await axios.post(`${process.env.CONFIRM_CODE_URL}`,data);
+  } catch(error){
+    console.log("Ошибка сервера (check_user_yes)");
+    return res.sendStatus(500);
+  }
   console.log(user.data()["entryDate"]);
-  if (
+  /*if (
     user.exists &&
     user.data().phoneNumber === digitsOnlyPhoneNum &&
     digitsOnlyCode === "7777"
-  ) {
+  ) */
+  const updateData = {};
+  const currentDate = new Date();
+  console.log(currentDate.getTime());
+  updateData['entryDate'] = currentDate;
+  await userRef.update(updateData);
 
-    const updateData = {};
-    const currentDate = new Date();
-    console.log(currentDate.getTime());
-    updateData['entryDate'] = currentDate;
-    await userRef.update(updateData);
-
-    const context = {
-      name: 'projects/eknot-ktdq/agent/sessions/2CF3B4D976AD447DDAE6BB2C6034CCA533252650FF31791390F00F0DD1D5D821/contexts/logincheck',
-      lifespanCount: 100,
-      parameters: {
-        flag: 'true',
-      }
+  const context = {
+    name: 'projects/eknot-ktdq/agent/sessions/2CF3B4D976AD447DDAE6BB2C6034CCA533252650FF31791390F00F0DD1D5D821/contexts/logincheck',
+    lifespanCount: 100,
+    parameters: {
+      flag: 'true',
+    }
     };
-  res.send({fulfillmentText: `Приветствую Вас, ${user.data().name}.\n Для того чтобы ознакомиться с функциями бота произнесите или напишите \"Помощь\".`, outputContexts: [context] });
-  } else {
-    res.send({
-      fulfillmentText: `Вы ввели неправильный номер телефона или код, введите всё заново`,
-    });
-  }
+  res.send({fulfillmentText: `Приветствую Вас, ${displayName}.\n Для того чтобы ознакомиться с функциями бота произнесите или напишите \"Помощь\".`, outputContexts: [context] });
 };
 
 const format_code = (number) => {
