ca5dae985d96689d06818c05c5c40d5e3ada5df6
Draconi <erkin.klyshev@mail.ru>
2023-09-18 21:54:34 +0600

    Нурбек лох

--- routes/yandex-dialogs.js
+++ routes/yandex-dialogs.js
@@ -1,3 +1,5 @@
+const User = require("../models/user");
+
 const express = require("express");
 const aliceRouter = express.Router();
 const detectIntent = require("../df");
@@ -18,11 +20,24 @@ const hello = () => ({
 
 aliceRouter.post("/", async (req, res) => {
   const { request, session, version } = req.body;
+
   const { user_id } = session.user;
   const response = { version, session };
   let intentResponse;
   if (!request.original_utterance) {
     response.response = hello();
+    const newUser = new User({yandex_id: user_id });
+    await User.findOne({ yandex_id: user_id }), (error) => {
+      if (error) {
+        newUser.save((error) => {
+          if (error) {
+            console.error('Ошибка при сохранении пользователя:', error);
+          } else {
+            console.log('Пользователь успешно сохранен');
+          }
+        });
+      } 
+    };
   } else {
     intentResponse = await detectIntent(request.original_utterance, user_id);
     response.response = { text: intentResponse.fulfillmentText };
