230dc97466640599a777151f3c2c597afaaa187c
Draconi <erkin.klyshev@mail.ru>
2023-09-19 22:06:29 +0600

    Константы

--- constants/constants.js
+++ constants/constants.js
@@ -1,11 +1,20 @@
 const STATUS = [
-  { key: "1", value: "Новая" },
-  { key: "2", value: "В работе" },
-  { key: "3", value: "Исполнена" },
-  { key: "4", value: "Закрыта" },
-  { key: "5", value: "Отказана" },
-  { key: "6", value: "Отменена" },
+  { key: "1", value: "Новая", oid : "60e80f65e4e96aab27b91859" },
+  { key: "2", value: "В работе", oid : "60e80f65e4e96aab27b9185a"},
+  { key: "3", value: "Исполнена", oid : "60e80f65e4e96aab27b9185b" },
+  { key: "4", value: "Закрыта",  oid : "60e80f65e4e96aab27b9185c"},
+  { key: "5", value: "Отказана", oid : "610394d252d6e7081c8d2ddc" },
+  { key: "6", value: "Отменена", oid : "614030e83d7e393568a9229f" },
 ];
+
+const LOCATION = [
+  {value:"Квартира", oid : "60e7ea00e4e96aab27b9184d"},
+  {value: "Подъезд", oid : "60e7ea00e4e96aab27b9184e"},
+  {value: "Дом", oid : "60e7ea00e4e96aab27b9184f"},
+  {value: "Двор", oid: "60e7ea00e4e96aab27b91850"},
+  {value: "Паркинг", oid: "60e7ea00e4e96aab27b91851"},
+  {value: "Другое", oid: "60e7ea00e4e96aab27b91852"}
+]
 module.exports = {
   STATUS,
 };
