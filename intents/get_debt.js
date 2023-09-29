1d6194109c32ae75817d3b9b504b554939444465
Nurbek <117496813+NKey4@users.noreply.github.com>
2023-09-29 20:59:02 +0600

    Изменение

--- intents/get_debt.js
+++ intents/get_debt.js
@@ -1,31 +1,5 @@
 require("dotenv").config();
 
-const get_debt = async (res, queryResult, user_id) => {
-  try {
-    const user = await User.findOne({ _id: user_id });
-
-    if (!user) {
-      return res.sendStatus(400);
-    }
-
-    const debt = user.debt;
-
-    if (debt === undefined || debt === null || debt === "") {
-      res.send({
-        fulfillmentText: "Вы не имеете никакой задолженности.",
-      });
-    } else {
-      res.send({
-        fulfillmentText: `Ваша задолженность на данный момент составляет ${debt} тг`,
-      });
-    }
-  } catch (error) {
-    console.error(
-      "Ошибка при получении данных о задолженности из базы данных:",
-      error
-    );
-    res.send({ fulfillmentText: "Приношу извинения. Ошибка сервера." });
-  }
-};
+const get_debt = async (res, queryResult, user_id) => {};
 
 module.exports = get_debt;
