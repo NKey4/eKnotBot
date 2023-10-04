0cac7fa53d8cb49393afefb448caf0b2791e27e8
Draconi <erkin.klyshev@mail.ru>
2023-10-05 04:18:54 +0600

    ///

--- routes/yandex-dialogs.js
+++ routes/yandex-dialogs.js
@@ -24,9 +24,9 @@ aliceRouter.post("/", async (req, res) => {
   let intentResponse;
   if (!request.original_utterance) {
     if (Object.keys(state.user).length) {
-      //response.user_state_update = { fullName: null };
+      //jsonAnswer.user_state_update = { fullName: null };
       intentResponse = await detectIntent(`${state.user.fullName}`, user_id);
-      response.response = { text: intentResponse.fulfillmentText };
+      jsonAnswer.response = { text: intentResponse.fulfillmentText };
     } else {
       jsonAnswer.response = hello();
     }
@@ -37,26 +37,14 @@ aliceRouter.post("/", async (req, res) => {
       if (intentResponse.intentDisplayName === "Exit") {
         jsonAnswer.response.end_session = true;
       } else if (
-<<<<<<< HEAD
         intentResponse.intentDisplayName === "check_user_yes_code") 
         {
         try {
           const contextToFind = `projects/eknot-ktdq/agent/sessions/${user_id}/contexts/logincheck`;
           const foundContext = intentResponse.context.find(context => context.name === contextToFind)
           console.log(foundContext.parameters.fields.fullName.stringValue);
-          response.user_state_update = {
-            fullName: foundContext.parameters.fields.fullName.stringValue,
-=======
-        intentResponse.context[3] &&
-        intentResponse.context[3].parameters &&
-        intentResponse.context[3].parameters.fields &&
-        intentResponse.context[3].parameters.fields.fullName
-      ) {
-        try {
           jsonAnswer.user_state_update = {
-            fullName:
-              intentResponse.context[3].parameters.fields.fullName.stringValue,
->>>>>>> 24644915f1e2965d2e365874e22aa37efb97e156
+            fullName: foundContext.parameters.fields.fullName.stringValue,
           };
         } catch (error) {
           console.error(error);
