02937157a7e96fdf7f9d5cdffe75faf01f2ef335
Nurbek <117496813+NKey4@users.noreply.github.com>
2024-04-01 19:08:02 +0500

    AAA

--- main.js
+++ main.js
@@ -12,14 +12,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 mongoose
-  .connect(process.env.DB_URL, {
+  .connect(process.env.MONGO_DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then((res) => console.log("MongoDB connected"))
   .catch((err) => console.log(`Error connecting to the database: ${err}`));
 
-app.listen(3000, () => console.log("Server is running on port 3000"));
+app.listen(process.env.PORT || 3000, () =>
+  console.log("Server is running on port 3000")
+);
 
 app.use("/", aliceRouter);
 app.use("/webhook", dialogFlowRouter);
